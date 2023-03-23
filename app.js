2eb1925ad9d4f0c25cda4c463b2db56b9d14f1d1
millerjammer <millerjammer@gmail.com>
2023-03-22 17:35:04 -0700

    using template

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 //app.get("/", (req, res) => res.type('html').send(html));
 
 // Express Middleware for serving static files
-app.use(express.static(path.join(__dirname, 'scripts')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 //Send html
 app.get('/', function(req, res) {
